97bd2258d8737731df7f4547452c6ccb72dcb6b0
Sunil S Solunke <sssolunke@gmail.com>
2023-05-20 21:29:37 +0530

    Joining notes router and main file

--- app/main.py
+++ app/main.py
@@ -8,10 +8,27 @@ Entry point for Notes Application
 __author__ = "Sunil S S"
 __date__ = "2023/05/20"
 
-from fastapi import FastAPI, APIRouter, status
+from fastapi import FastAPI
+from app import models, notes
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine
+
+# Create tables/object strcuture as defined in the models.add()
+
+models.Base.metadata.create_all(bind=engine)
 
 # Initialize the app.
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(notes.router, tags=['Notes'], prefix='/api/notes')
 
 # GET Methoed for healh checking.
 @app.get("/api/healthchecker")
